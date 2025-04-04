7061d48f6aa809774abf39ea42eccd10efadd065
summatechno <isholadolapo57@gmail.com>
2025-04-04 04:41:28 +0100

    Add files via upload

---
+++ cooperative/script.js
@@ -0,0 +1,33 @@
+$(function () {
+
+  window.onscroll = function(){scrollFunction()};
+  function scrollFunction() {
+      if (document.body.scrollTop >= 50 || document.documentElement.scrollTop > 50) {
+          $(".header").css({"top":"0px", "width":"100%", "margin":"0", "background-color":"hsla(240, 100%, 21%)"});
+
+          $(".up").css({"bottom":"100px"});
+          
+      }
+      
+      else{
+        $(".header").css({"top":"30px", "width":"95%", "margin":"0 2.5%", "background-color":"hsla(240, 100%, 21%, 0.7)"});
+
+        $(".up").css({"bottom":"-100px"});
+      }
+  } 
+
+  $(".testimony .column .col .text").click(function () {
+    $(this).children(".testimony .column .col .text #p").slideToggle(200);
+    $(this).children(".testimony .column .col .text .span").html("READ LESS");
+  })
+  
+
+    $(".ham").click(function () {
+        $(".mobile").css({left:"0%"});
+    })
+    $(".mobile").click(function () {
+        $(".mobile").css({left:"-105%"});
+    })
+
+    $("#year").html(new Date().getFullYear());
+});
